9f9879d34261e990cb56d3e8252c8830c6f4885f
detailyang <detailyang@gmail.com>
2016-04-07 21:32:39 +0800

    use double quote

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const app = express();
+/**
+* @Author: BingWu Yang <detailyang>
+* @Date:   2016-04-07T17:52:53+08:00
+* @Email:  detailyang@gmail.com
+* @Last modified by:   detailyang
+* @Last modified time: 2016-04-07T18:23:47+08:00
+* @License: The MIT License (MIT)
+*/
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/*", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.post("/*", function (req, res) {
+  console.log("123123");
+  console.log(req.body);
+  res.send("Hello World!");
 });
 
 app.listen(3000, function () {
-  console.log('cas-gitlab listening on port 3000!');
+  console.log("cas-gitlab listening on port 3000!");
 });
